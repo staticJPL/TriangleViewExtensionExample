515645dcb8759b6b0fadcfe1778b5556e6184fa5
Julien Popa-Liesz <jpopaliesz@gmail.com>
2023-11-17 05:42:12 -0600

    Delete Source/TriangleShader directory

--- Source/TriangleShader/TriangleShader.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TriangleShader.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TriangleShader, "TriangleShader" );
- 
